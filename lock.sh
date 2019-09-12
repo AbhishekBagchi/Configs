5a2205aa0d793a5abf4c0005b349cb79470f82c2
Abhishek Bagchi <abhishekbagchi@icloud.com>
2019-09-12 17:43:55 +0100

    Change i3lock behaviour

--- lock.sh
+++ lock.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-icon="$HOME/Pictures/icon.png"
 tmpbg='/tmp/screen.png'
 
 (( $# )) && { icon=$1; }
 
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-i3lock -u -i "$tmpbg"
+i3lock -e -f -i "$tmpbg"
 rm $tmpbg
